4d0f79d2eb4a2184d53574df0d3e26e53c6e2dd0
“Ricardo” <ricardolopez.computing@gmail.com>
2017-11-19 19:15:09 -0600

    First component

---
+++ src/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2> {{titulo}} </h2>
+</template>
+
+
+<script>
+export default {
+  name: 'home',
+  data () {
+    return {
+      titulo: 'Home'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+/** Solo al importar el componente ya podemos usarlo    */
+import Home from './Home.vue'
+Vue.component('home', Home)
+
+
 new Vue({
   el: '#app',
   render: h => h(App)
