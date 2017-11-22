e814fb9f80b605ccfddb734b692fdff2ed2e45d5
“Ricardo” <ricardolopez.computing@gmail.com>
2017-11-21 23:33:24 -0600

    Delete restaurant
    interesante uso de showDelete

--- src/components/containers/RestauranteList.vue
+++ src/components/containers/RestauranteList.vue
@@ -10,6 +10,14 @@
             <router-link :to="{name:'editar-restaurante', params:{id:restaurante.id} }">
               editar
             </router-link>
+            <span v-if="showDelete != restaurante.id">
+              <a @click="borrarRestaurante(restaurante.id)"> Eliminar </a>
+            </span>
+            <span v-else>
+              <p> Estas seguro que qiuieres borrarlo </p>
+              <button @click="cancelarRestaurante()">Cancelar</button>
+              <button @click="confirmarRestaurante(restaurante.id)">Borrar</button>
+            </span>
           </p>
         </li>
       </ul>
@@ -29,7 +37,7 @@ export default {
     return {
       texto: 'Pagina Restaura tante',
       restaurantes : null,
-
+      showDelete: null
     }
   },
   methods: {
@@ -37,6 +45,19 @@ export default {
       axios.get('http://localhost:80/slim/restaurantes-api.php/restaurantes')
         .then( (res)=>{
           this.restaurantes = res.data.data
+        })
+    },
+    borrarRestaurante(id){
+      this.showDelete = id;
+    },
+    cancelarRestaurante(){
+      this.showDelete = null
+    },
+    confirmarRestaurante(id){
+      axios.get('http://localhost:80/slim/restaurantes-api.php/delete-restaurante/'+id)
+        .then( (res)=>{
+          this.showDelete = null
+          this.getRestaurantes() // Actualizar restaurantes quitando el eliminado
         })
     }
   }
